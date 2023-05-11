2e7e772fda5299aab4f9cdba2cbb1d379990f5b2
G284132022 <228413@st.takushoku-u.ac.jp>
2023-05-11 21:34:59 +0900

    ファイル設置

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -20,17 +20,17 @@ function hantei() {
   console.log(kaisu+"回目の予想:"+yoso);
   } if(kaisu>=4){
     console.log("答えは"+kotae+"でした。すでにゲームは終わっています。");
-  }else{
-      if(yoso===kotae){
+  } else if(yoso===kotae){
     console.log("正解です。おめでとうございます。");
-  } else if (yoso>kotae){
+  } if (yoso>kotae){
     console.log("残念、もっと小さいですよ。");
   } else {
     console.log("残念、もっと大きいですよ。");
   }
+  
   // 課題3-1: 正解判定する
   
   
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-}
+
