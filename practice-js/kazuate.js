44387ddeff149e869c2413abf1e7f50c5a4ec4ca
G284132022 <228413@st.takushoku-u.ac.jp>
2023-05-11 23:09:22 +0900

    ファイル設置

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -2,7 +2,7 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
-let kaisu = 0;
+
 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
@@ -15,21 +15,21 @@ hantei();
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
-  kaisu++;
+}let yoso = 4;
+  for( let kaisu=1;kaisu<=4;kaisu++){
   console.log(kaisu+"回目の予想:"+yoso);
- } if(kaisu===3){
+  if(kaisu===3){
     console.log("まちがい、残念でした答えは"+kotae);
-} else if (kaisu>=4){
+}  else if (kaisu>=4){
   console.log("答えは"+kotae+"でした。すでにゲームは終わっています。");
-}  else if (yoso<kotae){
-  console.log("残念、もっと小さいですよ。");
-}  else if(kotae>yoso){
+}   if (yoso===kotae){
+  console.log("おめでとう、正解です。");
+}  else if(kotae<yoso){
   console.log("残念、もっと大きいですよ。");
 } else {
-  console.log("おめでとう、正解です。");
+  console.log("残念、もっと小さいですよ。");
 } 
-
+}
   
   // 課題3-1: 正解判定する
   
