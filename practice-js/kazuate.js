a72988bb24474fb9e64df1d7109578b29b87fa81
G284132022 <228413@st.takushoku-u.ac.jp>
2023-05-13 11:30:29 +0900

    ファイル設置

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -2,7 +2,7 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
-
+let kaisu =0;
 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
@@ -16,20 +16,20 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
 }let yoso = 4;
-  for( let kaisu=1;kaisu<=4;kaisu++){
-  console.log(kaisu+"回目の予想:"+yoso);
-  if(kaisu===3){
-    console.log("まちがい、残念でした答えは"+kotae);
-}  else if (kaisu>=4){
-  console.log("答えは"+kotae+"でした。すでにゲームは終わっています。");
-}   if (yoso===kotae){
-  console.log("おめでとう、正解です。");
-}  else if(kotae<yoso){
-  console.log("残念、もっと大きいですよ。");
-} else {
-  console.log("残念、もっと小さいですよ。");
-} 
-}
+kaisu++;
+   console.log(kaisu+" 回目の予想"+yoso);
+   if(kaisu>=4){
+    console.log("答えは"+kotae+"でした。"+"ゲームはすでに終わっています。");
+   } else if (kaisu===3){
+    console.log("まちがい、残念でした答えは"+kotae+"です。");
+   } else if (kotae<yoso){
+    console.log("まちがい答えはもっと大きいですよ。");
+   }  if (kotae>yoso){
+    console.log("まちがい答えはもっと小さいですよ。");
+   } else {
+    console.log("正解です。おめでとう！");
+   }
+
   
   // 課題3-1: 正解判定する
   
