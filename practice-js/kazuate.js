2d306e6612625e0ed2ba33caf37f6c37e9b65344
G284132022 <228413@st.takushoku-u.ac.jp>
2023-05-16 21:25:13 +0900

    ファイル設置

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -3,7 +3,7 @@ console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu =0;
-
+ 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
@@ -17,18 +17,31 @@ function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
 }let yoso = 4;
 kaisu++;
-   console.log(kaisu+" 回目の予想"+yoso);
-   if(kaisu>=4){
-    console.log("答えは"+kotae+"でした。"+"ゲームはすでに終わっています。");
-   } else if (kaisu===3){
-    console.log("まちがい、残念でした答えは"+kotae+"です。");
-   }  if (kotae===yoso){
-    console.log("正解、です。おめでとう");
-   } else if (kotae>yoso){
-    console.log("まちがい答えはもっと小さいですよ。");
-   } else if (kotae<yoso) {
-    console.log("まちがい、答えはもっと大きいですよ。");
-   }
+  console.log(kaisu+"回目の予想:"+yoso);
+ if(kaisu>=4){
+  console.log("答えは"+kotae+"でした。ゲームは既に終わっています。");
+ }if(kaisu===3){
+  console.log("残念でした答えは"+kotae+"でした。");
+ }if(kaisu<3){
+  if(kotae>yoso){
+          console.log("残念、もっと小さいですよ。");
+  }else if (kotae<yoso){
+    console.log("残念、もっと大きいですよ。");
+  } else {
+    console.log("正解、おめでとう！");
+  }
+ }
+  
+  
+
+  // if(yoso===kotae){
+  //   console.log("正解です。おめでとうございます。");
+  // }
+  // else if(kotae>yoso ){
+  //   console.log("残念、もっと大きいです。");
+  // } else if(kotae<yoso){
+  //   console.log("残念、もっと小さいです。");
+  // }
 
   
   // 課題3-1: 正解判定する
