64a8a6f0b8c5e8defdf1700e98d308a8558962ab
G284132022 <228413@st.takushoku-u.ac.jp>
2023-06-01 16:41:08 +0900

    ファイル設置

--- kazuate.js
+++ kazuate.js
@@ -3,7 +3,9 @@ console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu =0;
- 
+let b1 = document.querySelector('button#print');
+b1.addEventListener('click', hantei);
+let judge =false;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
@@ -11,38 +13,46 @@ let kaisu =0;
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-  let yoso = 4;
+  let i = document.querySelector('input[name="shimei"]');
+    let yoso = Number(i.value);
+    kaisu++;
   let b = document.querySelector('p#result');   
-kaisu++;
-  console.log(kaisu+"回目の予想:"+yoso);
- if(kaisu>=4){
-  b.textContent=('答えは'+kotae+'でした。ゲームは既に終わっています。');
-  console.log("答えは"+ kotae +"でした。ゲームは既に終わっています。");
- }if(kaisu===3){
-    console.log("残念、答えは、"+kotae+"でした。");
-    b.textContent=("残念、答えは、"+kotae+"でした。");
- }
- if(kaisu<3){
-  if(kotae<yoso){
-    console.log("残念、もっと小さいよ");
-    b.textContent=('残念、もっと小さいよ');
-  } else if(kotae>yoso){
-    console.log("残念、もっと大きいですよ。");
-    b.textContent=('残念、もっと大きいですよ。');
-  }else {
-    console.log("正解、おめでとう！");
-    b.textContent=('正解、おめでとう！');
-  }if(yoso==kotae){
-    kaisu=4;
+let n =document.querySelector('span#kaisu'); 
+  n.textContent =kaisu;
+  let a =document.querySelector('span#answer'); 
+  a.textContent =yoso;
+  if(judge===false){
+    if(kaisu>=4){
+      b.textContent=('答えは'+kotae+'でした。ゲームは既に終わっています。');
+      console.log("答えは"+ kotae +"でした。ゲームは既に終わっています。");
+     }if(kaisu===3){
+      if(yoso===kotae){
+        b.textContent= ("正解です。おめでとう！");
+      }else{
+        b.textContent=("残念、答えは、"+kotae+"でした。");
+      }
+     }
+     if(kaisu<3){
+      if(kotae<yoso){
+        console.log("残念、もっと小さいよ");
+        b.textContent=('残念、もっと小さいよ');
+      } else if(kotae>yoso){
+        console.log("残念、もっと大きいですよ。");
+        b.textContent=('残念、もっと大きいですよ。');
+      }else {
+        console.log("正解、おめでとう！");
+        b.textContent=('正解、おめでとう！');
+        judge=true;
+      }
   }
+ }else{
+  b.textContent=('答えは'+kotae+'でした。ゲームは既に終わっています。');
  }
  
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
  
- let n =document.querySelector('span#kaisu'); 
-  n.textContent =kaisu;     
-  let a =document.querySelector('span#answer'); 
-  a.textContent =yoso;
+ 
+
                                                                                                                           
 }  
   
