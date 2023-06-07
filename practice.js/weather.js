1cb9b06d4e62b136844dfc2aac797c4846abe75a
G284132022 <228413@st.takushoku-u.ac.jp>
2023-06-07 20:10:48 +0900

    ファイル設置

--- practice.js/weather.js
+++ practice.js/weather.js
@@ -62,5 +62,19 @@ console.log("湿度"+n.humidity);
   console.log("風速"+c.speed);
   console.log("風向"+c.deg); 
 }
-let n =document.querySelector('p#ID'); 
-   n.textContent=data.id+data.name;
+
+   
+   let b = document.querySelector('button#btn');
+b.addEventListener('click', showSelectResult);
+
+function showSelectResult() {
+    let s = document.querySelector('select#santaro');
+    let idx = s.selectedIndex;  // idx 番目の option が選択された
+
+    let os = s.querySelectorAll('option');  // s の子要素 option をすべて検索
+    let o = os.item(idx);       // os の idx 番目の要素
+
+    console.log('選択された ' + idx + ' 番目の option の情報:');
+    console.log('  value=' + o.getAttribute('value'));  // id 属性を表示
+    console.log('  textContent='+o.textContent);
+}
